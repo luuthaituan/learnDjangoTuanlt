3a9c3fd39718f4aeaed6540732c1410bc35b1ae0
luuthaituan <luuthaituan08091999@gmail.com>
2024-01-05 17:12:26 +0700

    tạo model

--- myproject/home/models.py
+++ myproject/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class home(models.Model):
+    first_column = models.CharField(max_length=255)
+    second_column = models.CharField(max_length=255)
+
+    #tạo ra bảng với 2 trường first_column và second_column
+    #Python manage.py makemigrations
+    #python manage.py migrate
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,11 +74,17 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+load_dotenv()
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
